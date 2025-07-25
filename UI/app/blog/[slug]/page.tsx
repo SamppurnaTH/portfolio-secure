7d43f482ee4cfb8501232a03914ed6c661a3da2e
VENU THOTA <venuthota722@gmail.com>
2025-07-25 16:57:26 +0530

    Wipe repo clean: removed all files from Git tracking

--- UI/app/blog/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import BlogPostClient from './BlogPostClient';
-
-export default async function BlogPostDetail({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
-  return <BlogPostClient slug={slug} />;
-}
--- UI/app/project/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProjectDetailClient from './ProjectDetailClient';
-
-export default async function ProjectDetail({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <ProjectDetailClient id={id} />;
-}
